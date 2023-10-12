fd2325f0efd58879be75a3c9c03de7565fc8f550
Pushpraj Pawar <114383343+Pushprajpawar96@users.noreply.github.com>
2023-10-12 13:24:19 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
 
 // export const BASE_URL = 'http://localhost:4000/api/v1/';
 
-export const BASE_URL = 'https://mern-tour-booking-app-backend.onrender.com'
+export const BASE_URL = 'https://mern-tour-booking-app-backend.onrender.com/'
 
 
 
